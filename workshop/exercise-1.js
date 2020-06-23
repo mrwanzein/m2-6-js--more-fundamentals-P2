04f7f29ee9aaf09699b5aad253b47ce12a08c0d2
Mrwan Zein <mrwanzein@outlook.com>
2020-06-23 12:24:20 -0400

    Exercise-1 Answer

--- workshop/exercise-1.js
+++ workshop/exercise-1.js
@@ -12,3 +12,17 @@ let mostPopularFood = [
   'parmesan chicken',
   undefined,
 ];
+
+const fixList = (arr, first, last) => {
+  arr.unshift(first);
+  arr[arr.length-1] = last;
+  return arr
+};
+
+const printList = (arr) => {
+  arr.forEach((food, index) => {
+    console.log(`${index+1}. ${food}`);
+  });
+};
+
+printList(fixList(mostPopularFood, 'bean burritos', 'buffalo-flavored cauliflower'));
