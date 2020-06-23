2377068740e427648f482e53ff9ff066680e90b3
Mrwan Zein <mrwanzein@outlook.com>
2020-06-23 14:28:33 -0400

    Exercise-7 Answer

--- workshop/exercise-7.js
+++ workshop/exercise-7.js
@@ -10,7 +10,15 @@ let foodPairings = {
 
 // createRecommendations function
 
+const createRecommendations = (obj) => {
+  let arr = Object.keys(obj);
+  let finalArr = arr.map(x => `With ${x}, is it best to have ${obj[x]}`);
+  return finalArr;
+}
+
 // printRecommendations function
+
+const printRecommendations = (arr) => arr.forEach(x => console.log(x)); 
 
 // function call (done)
 printRecommendations(createRecommendations(foodPairings));
