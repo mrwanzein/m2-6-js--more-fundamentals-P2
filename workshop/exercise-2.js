952c17437711fe3622cd9a88546d2c6fa0fb0611
Mrwan Zein <mrwanzein@outlook.com>
2020-06-23 13:28:41 -0400

    Exercise-2 Answer

--- workshop/exercise-2.js
+++ workshop/exercise-2.js
@@ -3,3 +3,12 @@
 
 const lateComers = ['Felicia', 'Charles', 'Terry'];
 const earlyBirds = ['Ellen', 'Jerry', 'Jasmine'];
+
+const printGuestOrder = (early, late) => {
+    let fullParty = [...early, ...late];
+    fullParty.forEach((guest, index) => {
+        console.log(`${index+1}. ${guest}`);
+    });
+}
+
+printGuestOrder(earlyBirds, lateComers);
